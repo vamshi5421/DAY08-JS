492b237e04041c387e299a9f56ad8df9984cb09c
vamshi5421 <vamshinayak5421@gmail.com>
2021-10-05 01:08:28 +0530

    added other options

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const gLink = document.getElementById("glink");
 const btn = document.getElementById("btn");
 const downloadLink = document.getElementById("download-link"); 
+const embedAudio = document.getElementById("embed-audio");
+const embedVideo = document.getElementById("embed-video");
+const clear = document.querySelector(".clear");
 
 btn.addEventListener("click", generateLink);
 
@@ -33,10 +36,44 @@ function generateLink(e){
         }
 
         const copy = document.querySelector(".copy");
-        copy.addEventListener("click", ( )=> {
+        copy.addEventListener("click", ()=> {
             return copyText(downloadLink)
         })
+
+        const audio1 = '<audio width="300" height="32" controls="controls" src="';
+        const audio2 = '" type="audio/mp3"></audio>';
+        console.log(downloadLink.value);
+        embedAudio.value = `${audio1}${downloadLink.value}${audio2}`;
+
+        const copyAudio = document.querySelector(".copy-audio")
+        copyAudio.addEventListener("click", ()=> {
+            return copyText(embedAudio)
+        })
+
+        const getVideoLink = gLink.value
+        .replace("/view?usp=sharing", "");
+
+        const video1 = '<iframe src="';
+        const video2 = '/preview" width="560" height="315"></iframe>';
         
+        embedVideo.value = `${video1}${getVideoLink}${video2}`;
+
+        const copyVideo = document.querySelector(".copy-video");
+        copyVideo.addEventListener("click", ()=> {
+            return copyVideo(embedVideo)
+        })
          
+    }else{
+        alert("Please enter google drive link")
     }
+}
+
+clear.addEventListener("click", clearForm);
+
+function clearForm(e) {
+    e.preventDefault();
+    gLink.value = "";
+    downloadLink.value = "";
+    embedAudio.value = "";
+    embedVideo.value = "";
 }
